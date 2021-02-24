659ea464f925e3a7f03fb17f5db336867f4a8d3f
2018822 <2018822@08-A103-MJCRZDF.SD41.ADS>
2021-02-24 09:32:52 -0800

    dylan

--- Dylan M 3/src/DylanM3.java
+++ Dylan M 3/src/DylanM3.java
@@ -5,18 +5,73 @@ public class DylanM3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+// 1
+int x, y;
+x = 1;
+y = 4;
+		System.out.println(1/4);
+// a.) Answer: 0
+
+		System.out.println(4.0/2);
+// b.) Answer: 2.0
+			
+		System.out.println(1.0/4.0);
+// c.) Answer: 0.25
+			
+int, x, y;
+x = 4.0;
+y = 2;
+// d.) Syntax error.
+		
+double x, y;
+x = 1;
+y = 4;
+		System.out.println(x/y);
+// e.) Syntax error.
+	
+		
+// 2
+		double x, y:
+		x = 1.0;
+		y = 3.0;
+		System.out.println(1/3)
+			// a.) Answer: 0.3333333333
+
+	
+		
+// 3 
+		int x = (int) Math.PI;
+		System.out.println((int)Math.PI);
+// This one is just the example ^
+	
+		System.out.println((int)4.999);
+// a.) Answer = 4
+				 
+		System.out.println((double)0.999);
+// b.) Answer = 0.999
+				
+		System.out.println((double)8);
+// c.) Answer = 8.0
+				
+		System.out.println(((int)4.999)/((int)2.5));
+// d.) Answer = 2
+
+		System.out.println((int)4.5/0.09);
+// e.) Answer = 44.44444444444
+				
+		
 		Scanner sc = new Scanner(System.in);
 // 4
-		/*
+		
 // a.) AreaToRadius	
 		System.out.println("What is the area?");
 		double area = sc.nextDouble();
 double radius = Math.sqrt(area/Math.PI);
 		System.out.println("The radius of the circle is:" + radius);
-		*/
 		
 		
-		/*
+		
 // b.) DistFormula
 		System.out.println("What are the coordinates of the first point?");
 int x1, y1; 
@@ -28,11 +83,11 @@ x2 = sc.nextInt();
 y2 = sc.nextInt();
 double dis = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 		System.out.println("The distance between these points:" + dis);
-		*/
+		
 		
 // c.) MortgageCalculator
 
-		/*
+		
 		  System.out.println("What is your initial amount?");
 double P;
 P = sc.nextDouble();
@@ -47,7 +102,7 @@ N = sc.nextDouble();
 
 double A = P*Math.pow(( 1.00 + R ), N);
 		System.out.println("Your final amount is..." + A);
-		*/
+		
 		
 // d.) Dice Rolls
 		System.out.println("Rolling...");
